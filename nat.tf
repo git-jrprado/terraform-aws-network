a48b8460888989312fbf7e3812426a3aff49ad8c
git-jrprado <31750084+git-jrprado@users.noreply.github.com>
2020-01-08 15:46:28 -0300

    Update nat.tf
    removed nat gateway from module

--- nat.tf
+++ nat.tf
@@ -10,17 +10,3 @@ resource "aws_eip" "nat_eip" {
     )
   )}"
 }
-
-resource "aws_nat_gateway" "nat_gw" {
-  count         = "${var.multi_nat ? (length(data.aws_availability_zones.available.names) > var.max_az ? var.max_az : length(data.aws_availability_zones.available.names)) : 1}"
-  allocation_id = "${aws_eip.nat_eip.*.id[count.index]}"
-  subnet_id     = "${aws_subnet.public.*.id[count.index]}"
-
-  tags = "${merge(
-    var.tags,
-    map(
-      "Name", "${var.name}-NATGW-${count.index}",
-      "EnvName", "${var.name}"
-    )
-  )}"
-}
